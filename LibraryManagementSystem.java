e890baf2b468bc81e22835942aca1e304d390f89
Alexme116 <alexmoncada16@hotmail.com>
2025-02-25 23:58:37 -0600

    Main flow completed

--- LibraryManagementSystem.java
+++ LibraryManagementSystem.java
@@ -6,7 +6,7 @@ public class LibraryManagementSystem {
     public static void main (String[] args) {
         Scanner scanner = new Scanner(System.in);
         boolean isRunning = true;
-        boolean isUserType = true;
+        boolean isUserType = false;
 
         while (isRunning) {
             System.out.println("Library Management System");
@@ -19,6 +19,7 @@ public class LibraryManagementSystem {
 
             switch (choiceUserType) {
                 case 1:
+                    isUserType = true;
                     while (isUserType) {
                         System.out.println("Librarian System");
                         System.out.println("1. Add Book");
@@ -30,18 +31,58 @@ public class LibraryManagementSystem {
                         System.out.print("Choose an option: ");  
                         int librarianChoice = scanner.nextInt();
                         scanner.nextLine();
+
+                        switch (librarianChoice) {
+                            case 1: // addbook function
+                                break;
+                            
+                            case 2: // display all books function
+                                break;
+
+                            case 3: // remove book function
+                                break;
+
+                            case 4: // register patrons function
+                                break;
+
+                            case 5: // view patrons function
+                                break;
+
+                            case 6:
+                                isUserType = false;
+                                break;
+                        
+                            default:
+                                System.out.println("Invalid option. Please try again.");
+                        }
                     }
                     break;
                 
                 case 2:
+                    isUserType = true;
                     while (isUserType) {
                         System.out.println("User System");
                         System.out.println("1. Display All Books");
                         System.out.println("2. Search Book");
-                        System.out.println("6. Go back");
+                        System.out.println("3. Go back");
                         System.out.print("Choose an option: ");  
                         int userChoice = scanner.nextInt();
                         scanner.nextLine();
+
+                        switch (userChoice) {
+                            case 1: // display all books function
+                                break;
+
+                            case 2: // search book function
+                                break;
+
+                            case 3:
+                                isUserType = false;
+                                break;
+                        
+                            default:
+                                break;
+                        }
                     }
                     break;
                 
